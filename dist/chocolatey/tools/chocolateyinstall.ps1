0b11f8fb46b42b18960f3a60e7395ff4e50c5b96
Juan Garcia Longaron <juan.garcia@mobilife.es>
2026-07-24 00:20:48 +0200

    chore: packaging manifests v0.6.1 [skip ci]

--- dist/chocolatey/tools/chocolateyinstall.ps1
+++ dist/chocolatey/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'confold'
-$url64       = 'https://github.com/confold/confold/releases/download/v0.6.0/Confold_0.6.0_x64-setup.exe'
-$checksum64  = '74EC138182382A352C9E75AB8FB7CDC302CA0E0DBA4889E89EE8AD8AB31570E6'
+$url64       = 'https://github.com/confold/confold/releases/download/v0.6.1/Confold_0.6.1_x64-setup.exe'
+$checksum64  = 'D38ED3425C425B094623C7C88DEEFD61E98A3D23C5C146902A6A373D89E875AE'
 
 # Downloads the official Tauri NSIS installer, verifies its SHA256, and installs it silently.
 # Chocolatey's auto-uninstaller picks up the Add/Remove Programs entry NSIS registers, so no
